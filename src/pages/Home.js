7c8c76ab48934d4a0b5e726acc8d0451d0ddb6f1
Asad <axsad398@gmail.com>
2023-07-03 22:29:29 +0500

    Adding Server link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
  const {user} = useAuthContext()
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts', {
+      const response = await fetch('https://gym-buddy-server.vercel.app/api/workouts', {
         headers: {
           'Authorization': `Bearer ${user.token}`
         }
