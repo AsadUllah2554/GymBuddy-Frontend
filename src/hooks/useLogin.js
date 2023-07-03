40fa7768ca4f999f85ca5a80d25206fdb4b9ea11
Asad <axsad398@gmail.com>
2023-07-03 19:04:43 +0500

    Update useLogin.js

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -10,7 +10,7 @@ export const useLogin = () => {
     const login = async (email,password) => {
         setIsLoading(true);
         setError(null);
-        const response = await fetch('/api/user/login',{
+        const response = await fetch('https://gym-buddy-server.vercel.app/api/user/login',{
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
